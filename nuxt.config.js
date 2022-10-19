e060225bc254a23f4858ebedb140dbb5c94d9313
Gokul Chowdary <ggokulchowdary@gmail.com>
2022-10-19 05:17:15 -0700

    Final commit3

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ const nuxtConfig = {
       { property: 'twitter:image', content: `${config.image}` },
     ],
     link: [
-      { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/apple-touch-icon.png' },
       { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css'}
     ],
   },
@@ -129,7 +129,7 @@ const nuxtConfig = {
 
   pwa: {
     icon: {
-      fileName: 'favicon.ico'
+      fileName: '/favicon.ico'
     },
     meta: {
       name: config.domain,
